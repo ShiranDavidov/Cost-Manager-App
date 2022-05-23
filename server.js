433ce6a7c996c83138bb32117217f8d902482874
shiranda <shirandavidov12@gmail.com>
2022-05-23 21:08:06 +0300

    add function

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express")
 const app = express()
 
 app.set("view engine","ejs")
+app.use(logger)
 
-app.get('/',(req, res)=>{
-    console.log('Here')
+app.get("/", logger,(req, res)=>{
     res.render("index", {text: "world"})
-
 })
 
 const userRouter = require("./routes/users")
 
 app.use("/users", userRouter)
 
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 app.listen(3000)
