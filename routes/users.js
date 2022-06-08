c3727e5877d9c7e4bbb538cf004f852662cb022c
DavidRimon241194 <davidr241194@gmail.com>
2022-06-08 11:40:58 +0300

    cuurent updated app

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,44 @@ const { route } = require('express/lib/application')
 const router = express.Router()
 const userModel = require('../models/Users')
 
-router.get("/", (req, res) => {
-    res.send("User list")
+
+router.get("/", async (req, res) => {
+    //res.send("User list")
+    var users = await userModel.find({});
+
+    try {
+      res.send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
 })
 
 router.post("/", async (req, res) => {
     //res.send('Create User')
-    const user = new userModel(req.body);
-  
+    //var reqBody = JSON.stringify(req.body);
+    const user = new userModel({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      birthday: req.body.birthday,
+      marital_status: req.body.marital_status,
+      occupation: {
+          comapny: req.body.occupation.comapny,
+          role: req.body.occupation.role
+      }
+    })
     try {
-      await user.save();
-      res.send(user);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+      const newUser = await user.save()
+      res.status(201).json(newUser)
+        /*first_name: String,
+        last_name: String,
+        birthday: Date,
+        marital_status: String,
+        occupation: occupationSchema*/
+      console.log(user)
+  } catch (e) {
+    res.status(400).json({message: err.message})
+    console.log(e.message)
+  }
 })
 
 router.get("/new", (req, res) => {
@@ -26,9 +50,15 @@ router.get("/new", (req, res) => {
 
 router
   .route("/:id")
-  .get((req, res) => {
+  .get( async (req, res) => {
     console.log(req.user)
-    res.send(`Get User With ID ${req.params.id}`)
+    //res.send(`Get User With ID ${req.params.id}`)
+    var user = await userModel.findOne({"_id":req.params.id});
+    try {
+      res.send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   })
   .put((req, res) => {
     res.send(`Update User With ID ${req.params.id}`)
