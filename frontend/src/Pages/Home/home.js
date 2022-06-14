8bdd9d79347ddcf0d697038d2775acaf36e3b265
ShiranDavidov <95073733+ShiranDavidov@users.noreply.github.com>
2022-06-14 12:50:44 +0300

    Add files via upload

---
+++ frontend/src/Pages/Home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./home.css";
+
+
+export default function Home() {
+
+  return (
+    <div class="home">
+    <h1> WELCOME TO OUR Cost Manager REStful Web Services &#128274; </h1>
+    </div>
+  );
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <App />
+,document.getElementById('root')
+);
